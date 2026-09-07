bd0a0d0338067c79b0e54fe1169e9a2cbb56d811
ksckaan1 <me@kaanksc.com>
2026-09-07 22:25:15 +0300

    fix: remove SplitHostPort from grpccomp that fails on :port format

--- components/grpccomp/grpccomp.go
+++ components/grpccomp/grpccomp.go
@@ -42,20 +42,15 @@ func (g *GRPC) Run(ctx context.Context) error {
 	if g.Reflect {
 		reflection.Register(g.server)
 	}
+
 	listener, err := net.Listen("tcp", g.Addr)
 	if err != nil {
 		return fmt.Errorf("net.Listen: %w", err)
 	}
 
-	host, port, err := net.SplitHostPort(g.Addr)
-	if err != nil {
-		return fmt.Errorf("net.SplitHostPort: %w", err)
-	}
-
 	logger.Default.Info(
 		ctx, "grpc server listening",
-		"host", host,
-		"port", port,
+		"addr", g.Addr,
 	)
 
 	err = g.server.Serve(listener)
