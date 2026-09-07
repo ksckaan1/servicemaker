08c1fd7332896ad4a568b20d84f00b6027a76755
ksckaan1 <me@kaanksc.com>
2026-09-07 22:18:29 +0300

    feat: add CORS, recover middleware and request logger to fibercomp
    - FIBER_USE_RECOVER_MW: panic recovery middleware (default: true)
    - FIBER_CORS_*: CORS configuration via env vars
    - Built-in REST request logger middleware

--- components/fibercomp/fiber.go
+++ components/fibercomp/fiber.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/gofiber/fiber/v3/middleware/recover"
 	"github.com/ksckaan1/logger"
 )
 
@@ -13,13 +15,22 @@ type Fiber struct {
 	router *fiber.App
 
 	// CONFIGS
-	BodyLimit                    int    `env:"FIBER_BODY_LIMIT" envDefault:"0"`
-	MaxRanges                    int    `env:"FIBER_MAX_RANGES" envDefault:"0"`
-	Concurrency                  int    `env:"FIBER_CONCURRENCY" envDefault:"0"`
-	StreamRequestBody            bool   `env:"FIBER_STREAM_REQUEST_BODY" envDefault:"false"`
-	DisablePreParseMultipartForm bool   `env:"FIBER_DISABLE_PRE_PARSE_MULTIPART_FORM" envDefault:"false"`
-	Addr                         string `env:"FIBER_ADDR" envDefault:":8080"`
-	DisableStartupMessage        bool   `env:"FIBER_DISABLE_STARTUP_MESSAGE" envDefault:"true"`
+	BodyLimit                    int      `env:"FIBER_BODY_LIMIT" envDefault:"0"`
+	MaxRanges                    int      `env:"FIBER_MAX_RANGES" envDefault:"0"`
+	Concurrency                  int      `env:"FIBER_CONCURRENCY" envDefault:"0"`
+	StreamRequestBody            bool     `env:"FIBER_STREAM_REQUEST_BODY" envDefault:"false"`
+	DisablePreParseMultipartForm bool     `env:"FIBER_DISABLE_PRE_PARSE_MULTIPART_FORM" envDefault:"false"`
+	Addr                         string   `env:"FIBER_ADDR" envDefault:":8080"`
+	DisableStartupMessage        bool     `env:"FIBER_DISABLE_STARTUP_MESSAGE" envDefault:"true"`
+	UseRecoverMW                 bool     `env:"FIBER_USE_RECOVER_MW" envDefault:"true"`
+	CORSAllowedOrigins           []string `env:"FIBER_CORS_ALLOWED_ORIGINS"`
+	CORSAllowedMethods           []string `env:"FIBER_CORS_ALLOWED_METHODS" envDefault:"GET,POST,PUT,DELETE,PATCH,OPTIONS"`
+	CORSAllowedHeaders           []string `env:"FIBER_CORS_ALLOWED_HEADERS" envDefault:"Origin,Content-Type,Accept,Authorization,Cookie"`
+	CORSExposeHeaders            []string `env:"FIBER_CORS_EXPOSE_HEADERS" envDefault:"Content-Length,Content-Type,Set-Cookie"`
+	CORSMaxAge                   int      `env:"FIBER_CORS_MAX_AGE" envDefault:"86400"`
+	CORSAllowCredentials         bool     `env:"FIBER_CORS_ALLOW_CREDENTIALS" envDefault:"false"`
+	CORSDisableValueRedaction    bool     `env:"FIBER_CORS_DISABLE_VALUE_REDUCTION" envDefault:"false"`
+	CORSAllowPrivateNetwork      bool     `env:"FIBER_CORS_ALLOW_PRIVATE_NETWORK" envDefault:"false"`
 }
 
 func (f *Fiber) Init(ctx context.Context) error {
@@ -36,6 +47,28 @@ func (f *Fiber) Init(ctx context.Context) error {
 			return json.Unmarshal(data, v)
 		},
 	})
+
+	if f.UseRecoverMW {
+		f.router.Use(recover.New())
+	}
+
+	if len(f.CORSAllowedOrigins) > 0 {
+		f.router.Use(cors.New(cors.Config{
+			AllowOrigins:          f.CORSAllowedOrigins,
+			AllowMethods:          f.CORSAllowedMethods,
+			AllowHeaders:          f.CORSAllowedHeaders,
+			ExposeHeaders:         f.CORSExposeHeaders,
+			MaxAge:                f.CORSMaxAge,
+			DisableValueRedaction: f.CORSDisableValueRedaction,
+			AllowCredentials:      f.CORSAllowCredentials,
+			AllowPrivateNetwork:   f.CORSAllowPrivateNetwork,
+		}))
+	}
+
+	f.router.Use(f.restLoggerMW)
+
+	logger.Default.Info(ctx, "fiber initialized")
+
 	return nil
 }
 
